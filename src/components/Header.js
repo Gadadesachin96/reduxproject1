b521e8b0100a9839b127078e9c61def00eaa9083
Sachin Gadade <gadadesachin51@gmail.com>
2023-06-09 12:08:45 +0530

    first

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div className="uifixed_menu">
+        <div className='ui container center'>
+            <h2>  Superbazaar </h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
